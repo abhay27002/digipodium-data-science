426fa1b561cac5aa7de63e9a8f1f2d61454c1c52
abhay <agarwalabhay2703@gmail.com>
2023-06-21 16:59:38 +0530

    functions

---
+++ polygon.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+def triangle():
+    for i in range(3):
+        fd(100)
+        lt(120)
+
+def square():
+    for i in range(4):
+        fd(100)
+        lt(90)
+
+def hexagon(d):
+    for i in range(6):
+        fd(d)
+        lt(60)
+
+def pentagon():
+    for i in range(5):
+        fd(100)
+        lt(75)
+
+def septagon():
+    for i in range(360/7):
+
+def octagon():
+    for i in range():
+        lt(100)
+        fd()
+mainloop()
+        
---
+++ t10.py
@@ -0,0 +1,12 @@
+from turtle import *
+from polygon import *
+
+speed('fastest')
+
+for i in range(6):
+  hexagon(100)
+  for i in range(6):
+    hexagon
+
+hideturtle()
+mainloop()
